39c4b7feb5e2de1ec9bbc95537bc3dbc80f0ade3
AW0723 <64997746+AW0723@users.noreply.github.com>
2023-07-20 17:51:51 +0800

    Part 21 completed

--- Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
+++ Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region State Variables
     public PlayerStateMachine StateMachine { get; private set; }
 
     public PlayerIdleState IdleState { get; private set; }
     public PlayerMoveState MoveState { get; private set; }
 
-    public Animator Anim { get; private set; }
-
     [SerializeField]
     private PlayerData playerData;
+    #endregion
 
+    #region Component Variables
+    public Animator Anim { get; private set; }
+    public PlayerInputHandler InputHandler { get; private set; }
+    public Rigidbody2D RB { get; private set; }
+    #endregion
+
+    #region Other Variables
+    public Vector2 CurrentVelocity { get; private set; }
+    public int FacingDirection { get; private set; }
+
+    private Vector2 workspace;
+    #endregion
+
+    #region Unity Callback Functions
     private void Awake()
     {
         StateMachine = new PlayerStateMachine();
@@ -26,12 +40,17 @@ public class Player : MonoBehaviour
     private void Start()
     {
         Anim = GetComponent<Animator>();
+        InputHandler = GetComponent<PlayerInputHandler>();
+        RB = GetComponent<Rigidbody2D>();
+
+        FacingDirection = 1;
 
         StateMachine.Initialize(IdleState);
     }
 
     private void Update()
     {
+        CurrentVelocity = RB.velocity;
         StateMachine.CurrentState.LogicUpdate();
     }
 
@@ -39,5 +58,32 @@ public class Player : MonoBehaviour
     {
         StateMachine.CurrentState.PhysicsUpdate();
     }
+    #endregion
 
+    #region Set Functions
+    public void SetVelocityX(float velocity)
+    {
+        workspace.Set(velocity, CurrentVelocity.y);
+        RB.velocity = workspace;
+        CurrentVelocity = workspace;
+    }
+    #endregion
+
+    #region Check Functions
+    public void CheckIfShouldFlip(int xInput)
+    {
+        if(xInput != 0 && xInput != FacingDirection)
+        {
+            Flip();
+        }
+    }
+    #endregion
+
+    #region Other Functions
+    private void Flip()
+    {
+        FacingDirection *= -1;
+        transform.Rotate(0f, 180f, 0f);
+    }
+    #endregion
 }
