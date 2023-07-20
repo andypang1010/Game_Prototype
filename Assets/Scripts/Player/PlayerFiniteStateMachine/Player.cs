6a14c9014482c9444fea79ab3447c52e5f9d32c0
AW0723 <64997746+AW0723@users.noreply.github.com>
2023-07-20 17:06:34 +0800

    StateMachine scripts added

--- Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
+++ Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerStateMachine StateMachine { get; private set; }
+
+    public PlayerIdleState IdleState { get; private set; }
+    public PlayerMoveState MoveState { get; private set; }
+
+    public Animator Anim { get; private set; }
+
+    [SerializeField]
+    private PlayerData playerData;
+
+    private void Awake()
     {
-        
+        StateMachine = new PlayerStateMachine();
+
+        // TODO: animation not yet created
+        IdleState = new PlayerIdleState(this, StateMachine, playerData, "idle");
+        MoveState = new PlayerMoveState(this, StateMachine, playerData, "move");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Anim = GetComponent<Animator>();
+
+        StateMachine.Initialize(IdleState);
     }
+
+    private void Update()
+    {
+        StateMachine.CurrentState.LogicUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        StateMachine.CurrentState.PhysicsUpdate();
+    }
+
 }
